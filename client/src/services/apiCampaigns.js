beefa0f6f080ff9f6fc8899a24a8f48a77827d6b
Adham <muzaffarov.adham@gmail.com>
2023-07-21 05:47:34 +0500

    feat: add services

---
+++ client/src/services/apiCampaigns.js
@@ -0,0 +1,19 @@
+const API_URL = "http://localhost:8000/api";
+
+export async function getCampaigns() {
+  const res = await fetch(`${API_URL}/campaigns`);
+
+  if (!res.ok) throw new Error("Failed getting campaigns");
+
+  const { campaigns } = await res.json();
+  return campaigns;
+}
+
+export async function getCampaignBySlug(slug) {
+  const res = await fetch(`${API_URL}/campaigns/${slug}`);
+
+  if (!res.ok) throw new Error("Couldn't find campaign");
+
+  const { campaign } = await res.json();
+  return campaign;
+}
